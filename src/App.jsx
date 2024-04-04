36528463d7fb18d39b7d2948c7d23fd170d81a66
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2024-04-04 16:26:44 -0400

    created pagenav component that lets us navigate throught he dirrent pages

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
-
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="*" element={<PageNotFound />} />
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
 function Homepage() {
   return (
     <div>
+      <PageNav />
       <h1>TravelTracer</h1>
-      <Link to="/pricing">Pricing</Link>
     </div>
   );
 }
