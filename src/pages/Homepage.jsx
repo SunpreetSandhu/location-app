9116e25c33be0e18420d411ece107955ea2f15af
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2024-04-04 16:11:44 -0400

    added link to product page

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,11 @@
+import { Link } from "react-router-dom";
 function Homepage() {
-  return <div>TravelTracer</div>;
+  return (
+    <div>
+      <h1>TravelTracer</h1>
+      <Link to="/pricing">Pricing</Link>
+    </div>
+  );
 }
 
 export default Homepage;
