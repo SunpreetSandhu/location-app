e68152c99a39ea42b65b7a519bfb0ba5699c8ae3
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2024-04-09 13:38:54 -0400

    added city list component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Pricing from "./pages/Pricing";
 import AppLayout from "./pages/AppLayout";
 import Product from "./pages/Product";
 import Login from "./pages/Login";
+import CityList from "./components/CityList";
 function App() {
   return (
     <BrowserRouter>
@@ -13,8 +14,8 @@ function App() {
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<p>List of cities</p>} />
-          <Route path="cities" element={<p> List of cities</p>} />
+          <Route index element={<CityList />} />
+          <Route path="cities" element={<CityList />} />
           <Route path="countries" element={<p>Countries</p>} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
