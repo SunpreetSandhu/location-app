8a292ce66c0e484acea0c72818d8f0928ef8d028
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2024-04-09 15:12:36 -0400

    rendered countries currently still cities but in countries styling

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import AppLayout from "./pages/AppLayout";
 import Product from "./pages/Product";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
 function App() {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -41,7 +42,10 @@ function App() {
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="countries" element={<p>Countries</p>} />
+          <Route
+            path="countries"
+            element={<CountryList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="form" element={<p>Form</p>} />
         </Route>
         <Route path="login" element={<Login />} />
---
+++ src/components/CountryList.jsx
@@ -0,0 +1,26 @@
+import CityItem from "./CityItem";
+import styles from "./CountryList.module.css";
+import Spinner from "./Spinner";
+import Message from "./Message";
+import CountryItem from "./CountryItem";
+function CountryList({ cities, isLoading }) {
+  if (isLoading) {
+    return <Spinner />;
+  }
+  if (!cities.length) {
+    return (
+      <Message message="Add your first city by clicking on a city on the map!" />
+    );
+  }
+
+  const countries = [];
+  return (
+    <ul className={styles.countryList}>
+      {cities.map((country) => (
+        <CountryItem country={country} key={country.id} />
+      ))}
+    </ul>
+  );
+}
+
+export default CountryList;
