57312156161c13b6be97350e3dfe60f7ff105a88
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2024-04-09 15:25:08 -0400

    checked for duplicate country names and displayed unique countries on ui

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -13,10 +13,15 @@ function CountryList({ cities, isLoading }) {
     );
   }
 
-  const countries = [];
+  const countries = cities.reduce((arr, city) => {
+    if (!arr.map((el) => el.country).includes(city.country))
+      return [...arr, { country: city.country, emoji: city.emoji }];
+    else return arr;
+  }, []);
+
   return (
     <ul className={styles.countryList}>
-      {cities.map((country) => (
+      {countries.map((country) => (
         <CountryItem country={country} key={country.id} />
       ))}
     </ul>
