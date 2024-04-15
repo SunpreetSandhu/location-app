0347a04a48c96ed7862fc2c18c0cace721f78de0
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2024-04-15 18:23:58 -0400

    consumed citiescontext in citylist

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect, useContext } from "react";
 
 const CitiesContext = createContext();
 
@@ -33,4 +33,11 @@ function CitiesProvider({ children }) {
   );
 }
 
-export { CitiesProvider };
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined)
+    throw new Error("Cities context was uesed outside the citiesProvider");
+  return context;
+}
+
+export { CitiesProvider, useCities };
