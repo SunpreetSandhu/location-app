aecb8d2e4942327c4a1f37663fc70e06992a90e5
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2024-04-15 20:58:55 -0400

    enabled map zoom in and zoom out

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -13,11 +13,11 @@ function Map() {
       <MapContainer
         center={mapPosition}
         zoom={13}
-        scrollWheelZoom={false}
+        scrollWheelZoom={true}
         className={styles.map}
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          attribution='&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         <Marker position={mapPosition}>
