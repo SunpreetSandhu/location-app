539cecb121650ae5fbd7bb64ebdce0f9d1956d2f
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2024-04-16 22:31:30 -0400

    did login check

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,15 +2,22 @@ import styles from "./Login.module.css";
 import { useState } from "react";
 import PageNav from "../components/PageNav";
 import Button from "../components/Button";
+import { useAuth } from "../contexts/AuthContext";
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState("jack@example.com");
   const [password, setPassword] = useState("qwerty");
-
+  const { login } = useAuth();
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (email && password) {
+      login(email, password);
+    }
+  }
   return (
     <main className={styles.login}>
       <PageNav />
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
           <input
